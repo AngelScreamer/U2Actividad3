b2c27180cf221ad884e46abcc55c24d5003d59a7
Angel Samaniego <171G0634@rcarbonifera.tecnm.mx>
2020-09-25 16:31:03 -0500

    Subiendo arcivos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unidad2Actividad3.Models;
+namespace Unidad2Actividad3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancico.OrderBy(x => x.Nombre);
+            return View(villancico);
+        }
+        public IActionResult Villancicos(int? id)
+        {
+            if(id== null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancico.FirstOrDefault(x => x.Id == id);
+            if(villancico==null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(villancico);
+            }
+            
+        }
+    }
+}
